ce8aa0f242fcec077e963babca3527a5088ce161
Manuel Tejada <manute32@gmail.com>
2019-03-12 15:37:36 +0100

    Implement basic login activity with firebase authentication

--- app/src/main/java/com/example/rithmilogin/MainActivity.java
+++ app/src/main/java/com/example/rithmilogin/MainActivity.java
@@ -1,13 +1,75 @@
 package com.example.rithmilogin;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "Sign In";
+
+    private EditText mEmailInput;
+    private EditText mPasswordInput;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_main);
+
+        // Initialize FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+
+        /* Views */
+        mEmailInput = findViewById(R.id.emailInput);
+        mPasswordInput = findViewById(R.id.passwordInput);
+
+        // Buttons
+        findViewById(R.id.emailSignInButton).setOnClickListener(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
+    }
+
+    private void signIn(String email, String password) {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Log.d(TAG, "Successfully signed in user" + user);
+                            } else {
+                                Toast.makeText(MainActivity.this, "Sign in failed", Toast.LENGTH_SHORT).show();
+                                Log.d(TAG, "Error signing in");
+                            }
+                        }
+                    });
+    }
+
+    @Override
+    public void onClick(View v) {
+        signIn(mEmailInput.getText().toString(), mPasswordInput.getText().toString());
     }
 }
